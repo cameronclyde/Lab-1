7b91f00d909687cc7ff374d6d21baf8d7096279b
cameronclyde <112019828+cameronclyde@users.noreply.github.com>
2022-08-24 15:32:36 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************
+* Hello, World Script
+************************************
+* Cameron Clyde
+* 8/24/2022
+* CMSC 255 Section 001
+***********************************/
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Cameron Clyde");
+		System.out.println("Hello, World Script");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC 255 Section 001");
+		System.out.println();
+
+	}
+
+}
